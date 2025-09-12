416024ecddac3198348531cffbf5077dd64157c1
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-09-12 15:20:18 -0300

    Terminado Models

--- FastAPI/models.py
+++ FastAPI/models.py
@@ -1,6 +1,4 @@
 from sqlmodel import SQLModel, Field
-from typing import List
-from datetime import date
 
 
 class Categoria(SQLModel, table=True):
@@ -10,11 +8,14 @@ class Categoria(SQLModel, table=True):
 class Produto(SQLModel, table=True):
     id: int = Field(primary_key=True)
     nome: str = Field(index=True)
-    categoria: Categoria = Field(index=True)
+    categoria_id: str = Field(index=False)
     estoque: int = Field(index=False)
+    loja_id: int = Field(index=False)
 
 class Favorito(SQLModel, table= True):
-    id: 
+    id: int = Field(primary_key=True)
+    produto_id: int = Field(index=False)
+    cliente_id: int = Field(index=False)
 
 class Cliente(SQLModel, table=True):
     id: int = Field(primary_key=True)
@@ -23,9 +24,37 @@ class Cliente(SQLModel, table=True):
     email: str = Field(index=False)
     senha: str = Field(index=False)
 
-    favoritos: List[Favorito] = Field()
-    amigos: List[Cliente] = Field()
-    carrinho: List[Produto] = Field()
+class Carrinho(SQLModel, table=True):
+    id: int = Field(primary_key=True)
+    cliente_id: int = Field(index=False)
+    produto_id: int = Field(index=False)
+    qnt_produto: int = Field(index=False)
+
+class Amigo(SQLModel, table=True):
+    id:int = Field(primary_key=True)
+    amigo_de: int = Field(index=False)
+    amigo: int = Field(index=False)
+
+class Loja(SQLModel, table=True):
+    id:int = Field(primary_key=True)
+    nome: int = Field(index=False)
+    cnpj: str = Field(index=False)
+    senha: str = Field(index=False)
+    descricao: str = Field(index=False)
+
+class Comentario(SQLModel, table=True):
+    id:int = Field(primary_key=True)
+    conteudo: str | None = Field(index=False)
+    avaliacao: int = Field(index=False)
+    cliente_id: int = Field(index=False)
+    produto_id: int = Field(index=False)
+
+
+
+
+
+
+    
 
 
 
