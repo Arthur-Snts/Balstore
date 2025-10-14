d21eac1bc467cdbeba36c2d70f0e0d93ba0be8cf
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-10-14 14:24:17 -0300

    Footer

--- React/balstore/src/App.jsx
+++ React/balstore/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header_and_Footer/Header'
+import Footer from './components/Header_and_Footer/Footer';
 import CarouselBALSTORE from './components/carrossel';
 
 function App() {
@@ -23,6 +24,8 @@ function App() {
 
         <CarouselBALSTORE/>
       </main>
+
+      <Footer></Footer>
     </div>
   );
 }
--- React/balstore/src/components/Header_and_Footer/Footer.jsx
+++ React/balstore/src/components/Header_and_Footer/Footer.jsx
@@ -1,4 +1,8 @@
 import "./Footer.css"
+import Bal_Logo from "../../assets/BALstore.png"
+import Github from "../../assets/github.png"
+import "../Cores.css"
+
 
 export default function Footer (){
 
@@ -6,7 +10,35 @@ export default function Footer (){
     return(
         <>
             <footer>
+                <div className="esquerda">
+                    <img src={Bal_Logo} alt="Balstore_icon"  className='bal-icon'/>
+                    <p>Copyright© 2025 Balstore. All Rights Reserved</p>
+                </div>
+                <div className="direita">
+                    <div className="links">
+                        <h5>Links Rápidos</h5>
+                        <ul className='footer-list'>
+                            <li><a href="" className='footer-link'>Página Inicial</a></li>
+                            <li><a href="" className='footer-link'>Categorias</a></li>
+                            <li><a href="" className='footer-link'>Perfil</a></li>
+                            <li><a href="" className='footer-link'>Lista de Amigos</a></li>
+                            <li><a href="" className='footer-link'>Venda na Balstore</a></li>
+                            <li><a href="" className='footer-link'>Sobre Nós</a></li>
+                        </ul>
+                    </div>
+                    <div className="githubs">
+                        <img src={Github} alt="Github_icon"  className='git-icon'/>
+                        <ul className='footer-list'>
+                            <li><a href="" className='footer-link'>Arthur Alves</a></li>
+                            <li><a href="" className='footer-link'>Artur Dantas</a></li>
+                            <li><a href="" className='footer-link'>Eliton Johnys</a></li>
+                            <li><a href="" className='footer-link'>Victor Lucas</a></li>
+                            <li><a href="" className='footer-link'>Vitor Emanuel</a></li>
+                        </ul>
+                    </div>
+                </div>
                 
+
             </footer>
         </>
     )
