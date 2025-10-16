5171d6ed04db8c9f696c75acdba51f24f5c34c4f
Mr-Vitor <vitor.e.demedeiros@gmail.com>
2025-10-16 16:09:37 -0300

    Limpa

--- React/balstore/src/App.jsx
+++ React/balstore/src/App.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import Header from './components/Header_and_Footer/Header'
 import Footer from './components/Header_and_Footer/Footer';
 import CarouselBALSTORE from './components/carrossel';
-import Card from './components/Produtos/ProdutoCard';
 import MainPage from './pages/MainPage'
 
 function App() {
@@ -11,22 +10,7 @@ function App() {
 
   return (
     <div>
-      <Header status={userStatus} />
-
-      <main style={{ padding: "20px" }}>
-        <h2>Página Inicial</h2>
-        <p>Status atual: {userStatus}</p>
-
-        {/* Botões para mudar o status e ver o header mudando */}
-        <button onClick={() => setUserStatus("guest")}>Deslogar</button>
-        <button onClick={() => setUserStatus("customer")}>Logar como Usuário</button>
-        <button onClick={() => setUserStatus("seller")}>Logar como Lojista</button>
-
-        <CarouselBALSTORE/>
         <MainPage/>
-      </main>
-
-      <Footer></Footer>
     </div>
   );
 }
