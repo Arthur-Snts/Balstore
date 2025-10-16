08aa31644a1abeef0782870b8a16898f31a24db1
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-10-16 15:39:16 -0300

    Segmentação do Header.jsx

--- React/balstore/src/App.jsx
+++ React/balstore/src/App.jsx
@@ -1,31 +1,20 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Header from './components/Header_and_Footer/Header'
-import Footer from './components/Header_and_Footer/Footer';
-import CarouselBALSTORE from './components/carrossel';
+import Header_Lojist from './components/Header_and_Footer/Header_Lojist'
+import Header_Guest from './components/Header_and_Footer/Header_Guest'
+import Header_Client from './components/Header_and_Footer/Header_Client'
+
+
 
 function App() {
 
-  const [userStatus, setUserStatus] = useState("guest");
 
   return (
     <div>
-      <Header status={userStatus} />
+      
+      <Header_Guest/>
+      <Header_Client/>
+      <Header_Lojist/>
 
-      <main style={{ padding: "20px" }}>
-        <h2>Página Inicial</h2>
-        <p>Status atual: {userStatus}</p>
-
-        {/* Botões para mudar o status e ver o header mudando */}
-        <button onClick={() => setUserStatus("guest")}>Deslogar</button>
-        <button onClick={() => setUserStatus("customer")}>Logar como Usuário</button>
-        <button onClick={() => setUserStatus("seller")}>Logar como Lojista</button>
-
-        <CarouselBALSTORE/>
-      </main>
-
-      <Footer></Footer>
     </div>
   );
 }
