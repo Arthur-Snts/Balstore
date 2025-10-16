5de52539db95b7a894375f308d02a11e4fa79d59
Mr-Vitor <vitor.e.demedeiros@gmail.com>
2025-10-16 16:14:24 -0300

    Limpa 2

--- React/balstore/src/App.jsx
+++ React/balstore/src/App.jsx
@@ -1,8 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import Header from './components/Header_and_Footer/Header'
-import Footer from './components/Header_and_Footer/Footer';
-import CarouselBALSTORE from './components/carrossel';
 import MainPage from './pages/MainPage'
 
 function App() {
--- React/balstore/src/pages/MainPage.jsx
+++ React/balstore/src/pages/MainPage.jsx
@@ -1,11 +1,18 @@
 import CategoriaButton from '../components/Produtos/CategoriasButton'
 import CarrosselCategoria from '../components/Produtos/CarrosselCategoria'
+import HeaderGuest from '../components/Header_and_Footer/Header_Guest'
+import Footer from '../components/Header_and_Footer/Footer'
+import Carrossel from '../components/carrossel'
+
 
 export default function MainPage() {
     return (
         <>
-            <CategoriaButton/>
-            <CarrosselCategoria/>
+            <HeaderGuest/>
+                <Carrossel/>
+                <CategoriaButton/>
+                <CarrosselCategoria/>
+            <Footer/>
         </>
 
     )
